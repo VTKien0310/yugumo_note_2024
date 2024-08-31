08833b7380d377239d072140159d8cf32e81c903
KonKeRoR <trgkien.vu@gmail.com>
2024-08-31 18:29:04 +0700

    feat: Introduce Livewire login form component
    Replaced inline login form HTML with a Livewire component to enhance modularity and maintainability. Moved the form structure to `LoginFormLivewireComponent` and updated the login page to use this new component.

---
+++ app/Views/Livewire/Authentication/LoginFormLivewireComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Views\Livewire\Authentication;
+
+use Illuminate\View\View;
+use Livewire\Component;
+
+class LoginFormLivewireComponent extends Component
+{
+    public function render(): View
+    {
+        return view('livewire.authentication.login-form-livewire-component');
+    }
+}
--- resources/views/modules/authentication/pages/login-page.blade.php
+++ resources/views/modules/authentication/pages/login-page.blade.php
@@ -1,29 +1,5 @@
 <x-layouts.master-layout>
     <div class="w-dvw h-dvh flex flex-col justify-center items-center bg-primary-content">
-        <div class="card w-1/4 h-1/3 flex flex-col justify-center items-center bg-base-100 rounded-lg shadow-xl">
-
-            <h1 class="mb-10 text-3xl brand-name-font">YUGUMO</h1>
-
-            <x-form id="login-form" class="flex flex-col justify-around items-center">
-                <div class="w-full text-left flex flex-row justify-between items-center">
-                    <x-label for="email" class="mr-6"/>
-                    <x-input name="email" class="input input-bordered mb-4"/>
-                    @error('email')
-                    <div class="alert alert-error">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <div class="w-full text-left flex flex-row justify-between items-center">
-                    <x-label for="password" class="mr-6"/>
-                    <x-input name="password" type="password" class="input input-bordered"/>
-                    @error('password')
-                    <div class="alert alert-error">{{ $message }}</div>
-                    @enderror
-                </div>
-            </x-form>
-
-            <button type="submit" form="login-form" class="mt-8 btn btn-primary">Log in</button>
-
-        </div>
+        <livewire:authentication.login-form-livewire-component/>
     </div>
 </x-layouts.master-layout>
