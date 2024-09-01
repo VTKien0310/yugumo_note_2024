e2378c6c070949b0ca494349eba51298eb2f0f3a
KonKeRoR <trgkien.vu@gmail.com>
2024-09-01 11:09:26 +0700

    feat: Enhance login form validation and UI
    Updated the login form to include Livewire two-way data binding for 'email' and 'password' fields. Introduced form validation and error handling in the component class, with an error message displayed upon failed login attempts. Adjusted UI for better layout and responsiveness.

--- app/Views/Livewire/Authentication/LoginFormLivewireComponent.php
+++ app/Views/Livewire/Authentication/LoginFormLivewireComponent.php
@@ -7,8 +7,22 @@ use Livewire\Component;
 
 class LoginFormLivewireComponent extends Component
 {
+    public string $email = '';
+
+    public string $password = '';
+
     public function render(): View
     {
         return view('livewire.authentication.login-form-livewire-component');
+    }
+
+    public function loginAttempt()
+    {
+        $validated = $this->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        session()->flash('login-failed', 'Invalid email or password.');
     }
 }
--- resources/views/livewire/authentication/login-form-livewire-component.blade.php
+++ resources/views/livewire/authentication/login-form-livewire-component.blade.php
@@ -1,23 +1,32 @@
-<div class="card w-1/4 h-1/3 flex flex-col justify-center items-center bg-base-100 rounded-lg shadow-xl">
-    <h1 class="mb-10 text-3xl brand-name-font">YUGUMO</h1>
+<div class="card w-1/4 h-1/2 min-h-fit flex flex-col justify-center items-center bg-base-100 rounded-lg shadow-xl">
+    <h1 class="mb-16 text-4xl brand-name-font">YUGUMO</h1>
 
-    <x-form id="login-form" class="flex flex-col justify-around items-center">
-        <div class="w-full text-left flex flex-row justify-between items-center">
-            <x-label for="email" class="mr-6"/>
-            <x-input name="email" class="input input-bordered mb-4"/>
+    <x-form wire:submit="loginAttempt" id="login-form" class="flex flex-col justify-around items-center mb-8">
+        <div class="w-full flex flex-col justify-center items-center mb-4">
+            <div class="w-full text-left flex flex-row justify-between items-center mb-1">
+                <x-label for="email" class="mr-6"/>
+                <x-input wire:model="email" name="email" class="input input-bordered"/>
+            </div>
             @error('email')
             <div class="alert alert-error">{{ $message }}</div>
             @enderror
         </div>
 
-        <div class="w-full text-left flex flex-row justify-between items-center">
-            <x-label for="password" class="mr-6"/>
-            <x-input name="password" type="password" class="input input-bordered"/>
+        <div class="w-full flex flex-col justify-center items-center">
+            <div class="w-full text-left flex flex-row justify-between items-center mb-1">
+                <x-label for="password" class="mr-6"/>
+                <x-input wire:model="password" name="password" type="password" class="input input-bordered"/>
+            </div>
             @error('password')
             <div class="alert alert-error">{{ $message }}</div>
             @enderror
         </div>
     </x-form>
 
-    <button type="submit" form="login-form" class="mt-8 btn btn-primary">Log in</button>
+    <div class="w-2/3 flex flex-col justify-center items-center">
+        <button type="submit" form="login-form" class="w-full btn btn-primary mb-2">Log in</button>
+        @if(session()->has('login-failed'))
+            <div class="w-full alert alert-error">{{ session('login-failed') }}</div>
+        @endif
+    </div>
 </div>
