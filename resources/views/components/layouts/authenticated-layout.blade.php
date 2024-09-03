b60844b1a02378ce7c39bba798169289ad7580a4
KonKeRoR <trgkien.vu@gmail.com>
2024-09-03 11:24:44 +0700

    feat: Add solar icons and improve authenticated layout design
    Updated `composer.json` to include `codeat3/blade-solar-icons` for enhanced icon usage. Adjusted authenticated layout with a responsive grid, improved drawer functionality, and integrated the new solar icons for a better user interface experience.

--- resources/views/components/layouts/authenticated-layout.blade.php
+++ resources/views/components/layouts/authenticated-layout.blade.php
@@ -1,9 +1,13 @@
-<div class="drawer">
+<div class="drawer grid grid-cols-12 gap-0">
+    <label for="my-drawer" class="btn btn-ghost drawer-button w-fit h-fit col-start-1 col-end-2">
+        <x-solar-hamburger-menu-linear class="w-10 h-10"/>
+    </label>
     <input id="my-drawer" type="checkbox" class="drawer-toggle"/>
-    <div class="drawer-content">
+
+    <div class="drawer-content w-full col-start-2 col-end-13">
         {{ $slot }}
-        <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
     </div>
+
     <div class="drawer-side">
         <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
         <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
