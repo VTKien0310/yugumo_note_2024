45618c50ae945a57db408edd22194a7bf53760d1
KonKeRoR <trgkien.vu@gmail.com>
2024-09-04 17:24:49 +0700

    feat: Add conditional links to navbar in authenticated layout
    Display brand name as a link unless on 'notes.recent' route and add 'New note' button unless on 'notes.create' route. This improves navigation and user experience by providing quick access to key actions.

--- resources/views/components/layouts/authenticated-layout.blade.php
+++ resources/views/components/layouts/authenticated-layout.blade.php
@@ -1,12 +1,27 @@
 <div>
     <div class="navbar bg-primary shadow-xl">
-        <h1 class="brand-name-font flex-1 text-xl text-primary-content">YUGUMO</h1>
+
+        @if(request()->route()->named('notes.recent'))
+            <h1 class="brand-name-font flex-1 text-xl text-primary-content">YUGUMO</h1>
+        @else
+            <a href="{{ route('notes.recent') }}" class="brand-name-font flex-1 text-xl text-primary-content">YUGUMO</a>
+        @endif
+
         <div class="flex-none">
-            <label class="input input-bordered flex items-center gap-2">
+
+            @if(!request()->route()->named('notes.create'))
+                <a href="{{ route('notes.create') }}" role="button" class="btn btn-ghost text-primary-content">
+                    New note
+                </a>
+            @endif
+
+            <label class="input input-bordered flex items-center gap-2 ml-1">
                 <input type="text" class="grow" placeholder="Search"/>
                 <x-ionicon-search class="h-4 w-4"/>
             </label>
+
         </div>
+
     </div>
 
     <div>
