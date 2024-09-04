5099d7e8310a94d80635606c757b9ae432e07bac
KonKeRoR <trgkien.vu@gmail.com>
2024-09-04 14:28:48 +0700

    feat: Update UI layout and change session lifetime
    Revamped the authenticated layout to include a navbar with a search feature, replacing the sidebar. Switched from "blade-solar-icons" to "blade-ionicons" and extended session lifetime to 30 days.

--- resources/views/components/layouts/authenticated-layout.blade.php
+++ resources/views/components/layouts/authenticated-layout.blade.php
@@ -1,19 +1,15 @@
-<div class="drawer grid grid-cols-12 gap-0">
-    <label for="my-drawer" class="btn btn-ghost drawer-button w-fit h-fit col-start-1 col-end-2">
-        <x-solar-hamburger-menu-linear class="w-10 h-10"/>
-    </label>
-    <input id="my-drawer" type="checkbox" class="drawer-toggle"/>
-
-    <div class="drawer-content w-full col-start-2 col-end-13">
-        {{ $slot }}
+<div>
+    <div class="navbar bg-primary shadow-xl">
+        <h1 class="brand-name-font flex-1 text-xl text-primary-content">YUGUMO</h1>
+        <div class="flex-none">
+            <label class="input input-bordered flex items-center gap-2">
+                <input type="text" class="grow" placeholder="Search"/>
+                <x-ionicon-search class="h-4 w-4"/>
+            </label>
+        </div>
     </div>
 
-    <div class="drawer-side">
-        <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-        <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-            <!-- Sidebar content here -->
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
-        </ul>
+    <div>
+        {{ $slot }}
     </div>
 </div>
