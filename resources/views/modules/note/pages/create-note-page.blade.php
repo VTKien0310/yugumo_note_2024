ccf5ca3bb6d6b9dd5fcf61fbbf27614d1a8e2278
KonKeRoR <trgkien.vu@gmail.com>
2024-09-05 15:03:02 +0700

    feat: Change layout of create-note-page to improve usability
    Switched from a horizontal to a vertical layout to better accommodate different screen sizes. Added a heading to guide users in selecting note categories, which are now displayed centrally. This enhances the user experience by making the interface more intuitive and visually appealing.

--- resources/views/modules/note/pages/create-note-page.blade.php
+++ resources/views/modules/note/pages/create-note-page.blade.php
@@ -19,25 +19,29 @@
     <x-slot:pageTitle>Add</x-slot:pageTitle>
 
     <x-layouts.authenticated-layout>
-        <div class="w-full flex flex-row justify-start content-center items-center gap-5">
+        <div class="w-full flex flex-col justify-start content-center items-center">
 
-            @foreach($noteCategories as $noteCategory)
-                <div class="card bg-base-100 w-96 shadow-xl">
-                    <figure>
-                        <img
-                            src="{{ $noteCategory['img'] }}"
-                            alt="{{ $noteCategory['img_alt'] }}"
-                        />
-                    </figure>
-                    <div class="card-body">
-                        <h2 class="card-title">{{ $noteCategory['name'] }}</h2>
-                        <p>{{ $noteCategory['description'] }}</p>
-                        <div class="card-actions justify-center">
-                            <button class="btn btn-primary btn-block">Add</button>
+            <h1 class="text-4xl mb-10">Which note category would you like to add?</h1>
+
+            <div class="w-full flex flex-row justify-center content-center items-center gap-5">
+                @foreach($noteCategories as $noteCategory)
+                    <div class="card bg-base-100 w-96 shadow-xl">
+                        <figure>
+                            <img
+                                src="{{ $noteCategory['img'] }}"
+                                alt="{{ $noteCategory['img_alt'] }}"
+                            />
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">{{ $noteCategory['name'] }}</h2>
+                            <p>{{ $noteCategory['description'] }}</p>
+                            <div class="card-actions justify-center">
+                                <button class="btn btn-primary btn-block">Add</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
 
         </div>
     </x-layouts.authenticated-layout>
