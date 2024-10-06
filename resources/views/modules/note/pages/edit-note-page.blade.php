efd1edb4e1ebdb269b2647149f72fc444ee6a626
VTKien0310 <trgkien.vu@gmail.com>
2024-10-06 17:36:11 +0700

    ref: Rename Livewire component for editing notes
    Changed the Livewire component from 'edit-note-form-livewire' to 'edit-simple-note-form-livewire' to improve clarity. This update ensures the naming convention reflects the simplified functionality of the form.

--- resources/views/modules/note/pages/edit-note-page.blade.php
+++ resources/views/modules/note/pages/edit-note-page.blade.php
@@ -2,6 +2,6 @@
     <x-slot:pageTitle>{{ $note->title }}</x-slot:pageTitle>
 
     <x-layouts.authenticated-layout>
-        <livewire:edit-note-form-livewire :note="$note"/>
+        <livewire:edit-simple-note-form-livewire :note="$note"/>
     </x-layouts.authenticated-layout>
 </x-layouts.master-layout>
