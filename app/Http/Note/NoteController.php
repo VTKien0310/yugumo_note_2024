dd3dc324e503bec42b882c5f547a3d8f8f73fd25
VTKien0310 <trgkien.vu@gmail.com>
2024-10-06 17:40:23 +0700

    feat: Add authorization to note viewing
    Integrated a `ViewNoteAuthorizer` class to verify user access rights when viewing notes. This ensures that users can only view notes that they own, enhancing security and access control.

--- app/Http/Note/NoteController.php
+++ app/Http/Note/NoteController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Note;
 
 use App\Extendables\Core\Http\Controllers\WebController;
+use App\Features\Note\Authorizers\ViewNoteAuthorizer;
 use App\Features\Note\Models\Note;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 class NoteController extends WebController
 {
@@ -35,8 +37,10 @@ class NoteController extends WebController
     /**
      * GET /notes/:id
      */
-    public function show(Note $note): View
+    public function show(Note $note, Request $request, ViewNoteAuthorizer $viewNoteAuthorizer): View
     {
+        $viewNoteAuthorizer->handle($request->user(), $note);
+
         return view('modules.note.pages.edit-note-page', compact('note'));
     }
 }
