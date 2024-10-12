c87ef95b8255cde7c193ab4def3928e59fa0db7e
VTKien0310 <trgkien.vu@gmail.com>
2024-10-12 16:23:47 +0700

    feat: Add auto-save functionality to note edit form
    Introduced `UpdateNoteByIdAction` to handle saving updates on the fly. Utilized `wire:model.live.debounce.250ms` to enable automatic saving of title and content changes with a 250ms debounce period.

--- resources/views/livewire/note/edit-simple-note-form-livewire.blade.php
+++ resources/views/livewire/note/edit-simple-note-form-livewire.blade.php
@@ -2,16 +2,28 @@
 
 use Livewire\Volt\Component;
 use App\Features\Note\Models\Note;
+use App\Features\Note\Actions\UpdateNoteByIdAction;
 
 new class extends Component {
+    public string $id;
+
     public string $title;
 
     public string $content;
 
     public function mount(Note $note): void
     {
+        $this->id = $note->id;
         $this->title = $note->title;
         $this->content = $note->content;
+    }
+
+    public function updated(): void
+    {
+        app()->make(UpdateNoteByIdAction::class)->handle($this->id, [
+            'title' => $this->title,
+            'content' => $this->content,
+        ]);
     }
 }; ?>
 
@@ -19,11 +31,16 @@ new class extends Component {
     <x-form class="w-full flex flex-col justify-start items-center">
         <div class="w-full flex flex-col justify-start items-start mb-5">
             <x-label for="title" class="font-bold text-xs"/>
-            <x-input wire:model="title" name="title" class="input input-bordered w-full mt-1"/>
+            <x-input wire:model.live.debounce.250ms="title" name="title" class="input input-bordered w-full mt-1"/>
         </div>
         <div class="w-full flex flex-col justify-start items-start">
             <x-label for="content" class="font-bold text-xs"/>
-            <x-textarea wire:model="content" name="content" :rows="20" class="textarea textarea-bordered w-full mt-1"/>
+            <x-textarea
+                wire:model.live.debounce.250ms="content"
+                name="content"
+                :rows="20"
+                class="textarea textarea-bordered w-full mt-1"
+            />
         </div>
     </x-form>
 </div>
