d85239952c1ecea97a442e63f43df12b045cd92a
VTKien0310 <trgkien.vu@gmail.com>
2024-10-13 21:32:40 +0700

    feat: Initialize markdown editor directly in app.js
    Simplified the initialization of the markdown editor by removing the separate loadMarkdownEditor function. Now, the editor is instantiated directly, reducing unnecessary function calls and improving code readability.

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,32 +7,24 @@ import.meta.glob([
 import '@toast-ui/editor/dist/toastui-editor.css';
 import {Editor} from "@toast-ui/editor";
 
-let editor;
-
-const loadMarkdownEditor = () => {
-    editor = new Editor({
-        el: document.querySelector('#editor'),
-        height: '500px',
-        initialEditType: 'wysiwyg',
-        previewStyle: 'vertical',
-        events: {
-            change: () => {
-                document.querySelector('#editor').dispatchEvent(
-                    new CustomEvent('content-change', {
-                        detail: editor.getMarkdown()
-                    })
-                )
-            }
+let editor = new Editor({
+    el: document.querySelector('#editor'),
+    height: '500px',
+    initialEditType: 'wysiwyg',
+    previewStyle: 'vertical',
+    events: {
+        change: () => {
+            document.querySelector('#editor').dispatchEvent(
+                new CustomEvent('content-change', {
+                    detail: editor.getMarkdown()
+                })
+            )
         }
-    });
-}
+    }
+});
 
 document.addEventListener('livewire:init', () => {
     Livewire.on('advanced-note-content-updated', (event) => {
-        loadMarkdownEditor()
         editor.setMarkdown(event.noteContent, true)
     });
 });
-
-
-loadMarkdownEditor();
