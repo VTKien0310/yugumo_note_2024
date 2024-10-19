9ee8be8e7bba71a8ed3fcc479acfc8cd3b972224
VTKien0310 <trgkien.vu@gmail.com>
2024-10-19 16:08:18 +0700

    ref: Refactor note type case statements for enum usage
    This change replaces hardcoded note type IDs with enum values for better readability and maintainability. It ensures that note type checks are consistent with the enum definitions in `NoteTypeIdEnum`. This refactoring also helps prevent potential errors related to magic numbers.

--- resources/views/modules/note/pages/edit-note-page.blade.php
+++ resources/views/modules/note/pages/edit-note-page.blade.php
@@ -1,3 +1,5 @@
+@use(App\Features\NoteType\Enums\NoteTypeIdEnum)
+
 <x-layouts.master-layout>
     <x-slot:pageTitle>{{ $note->title }}</x-slot:pageTitle>
 
@@ -5,10 +7,10 @@
         <div class="w-full flex flex-col justify-start content-center items-center">
             <p class="mb-3 text-center text-sm font-bold">{{ $noteType->name }}</p>
             @switch($note->type_id)
-                @case(\App\Features\NoteType\Enums\NoteTypeIdEnum::SIMPLE->value)
+                @case(NoteTypeIdEnum::SIMPLE->value)
                     <livewire:edit-simple-note-form-livewire :note="$note"/>
                     @break
-                @case(\App\Features\NoteType\Enums\NoteTypeIdEnum::ADVANCED->value)
+                @case(NoteTypeIdEnum::ADVANCED->value)
                     <livewire:edit-advanced-note-form-livewire :note="$note"/>
                     @break
                 @default
