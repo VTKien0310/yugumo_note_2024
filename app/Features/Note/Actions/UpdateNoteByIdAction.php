f75069b26a415a62368aeeb115bcd716c723d6be
VTKien0310 <trgkien.vu@gmail.com>
2024-10-19 17:49:20 +0700

    ref: Refactor note content update logic
    Replaced direct call to UpdateTextNoteContentCommand with a new UpdateTextNoteContentAction to handle note content updates based on the note type. Introduced a private method to manage content updates conditionally, improving code modularity and readability.

--- app/Features/Note/Actions/UpdateNoteByIdAction.php
+++ app/Features/Note/Actions/UpdateNoteByIdAction.php
@@ -3,15 +3,15 @@
 namespace App\Features\Note\Actions;
 
 use App\Features\Note\Commands\UpdateNoteCommand;
-use App\Features\Note\Commands\UpdateTextNoteContentCommand;
 use App\Features\Note\Models\Note;
+use App\Features\NoteType\Enums\NoteTypeEnum;
 use Illuminate\Support\Facades\DB;
 
 readonly class UpdateNoteByIdAction
 {
     public function __construct(
         private UpdateNoteCommand $updateNoteCommand,
-        private UpdateTextNoteContentCommand $updateTextNoteContentCommand
+        private UpdateTextNoteContentAction $updateTextNoteContentAction
     ) {}
 
     public function handle(string $noteId, array $data): Note
@@ -21,9 +21,20 @@ readonly class UpdateNoteByIdAction
 
             $note = $this->updateNoteCommand->handle($note, $data);
 
-            $this->updateTextNoteContentCommand->handle($note->textContent, $data);
+            $this->updateNoteContentBasedOnNoteType($note, $data);
 
             return $note;
         });
+    }
+
+    private function updateNoteContentBasedOnNoteType(Note $note, array $data): void
+    {
+        match ($note->type_id) {
+            NoteTypeEnum::SIMPLE->value, NoteTypeEnum::ADVANCED->value => $this->updateTextNoteContentAction->handle(
+                $note->textContent,
+                $data['content']
+            ),
+            default => null
+        };
     }
 }
