9ef69eb642e8c3845aa8b4df6c790ba8e257c3f9
KonKeRoR <trgkien.vu@gmail.com>
2024-10-25 17:03:42 +0700

    feat: Refactor ChecklistItem component to streamline data handling
    Refactored the ChecklistItem Livewire component to improve data handling by introducing a new method to extract data from the checklist item. Updated variable naming for consistency and added debouncing to input fields to optimize performance.

--- resources/views/livewire/note/checklist-item-form-livewire.blade.php
+++ resources/views/livewire/note/checklist-item-form-livewire.blade.php
@@ -3,41 +3,50 @@
 use Livewire\Volt\Component;
 use App\Features\Note\Models\ChecklistNoteContent;
 use App\Features\Note\Actions\UpdateChecklistNoteContentAction;
+use App\Extendables\Core\Utils\BoolIntValueEnum;
 
 new class extends Component {
     public ChecklistNoteContent $checklistItem;
     public string $content;
-    public int $is_completed;
+    public int $isCompleted;
 
     public function mount(ChecklistNoteContent $checklistItem): void
     {
-        $this->checklistItem = $checklistItem;
-        $this->content = $checklistItem->content;
-        $this->is_completed = $checklistItem->is_completed->value;
+        $this->extractDataFromChecklistContent($checklistItem);
     }
 
     public function updated(): void
     {
         $checklistItem = app()->make(UpdateChecklistNoteContentAction::class)->handle($this->checklistItem, [
-            ChecklistNoteContent::CONTENT => $this->content
+            ChecklistNoteContent::CONTENT => $this->content,
+            ChecklistNoteContent::IS_COMPLETED => BoolIntValueEnum::from($this->isCompleted)
         ]);
 
+        $this->extractDataFromChecklistContent($checklistItem);
+    }
+
+    private function extractDataFromChecklistContent(ChecklistNoteContent $checklistItem): void
+    {
+        $this->checklistItem = $checklistItem;
         $this->content = $checklistItem->content;
+        $this->isCompleted = $checklistItem->is_completed->value;
     }
 }; ?>
 
-<label data-checklist-item-id="{{ $checklistItem->id }}" class="label cursor-pointer">
+<label class="label cursor-pointer">
     <input
         type="text"
+        wire:model.live.debounce.500ms="content"
         value="{{ $content }}"
-        @class(['text-decoration-line: line-through'=> $is_completed])
+        @class(['text-decoration-line: line-through'=> $isCompleted])
         class="input input-ghost"
         style="width: 100%;" {{-- workaround for style overriding from packages and libraries --}}
     />
     <div class="flex flex-row justify-end items-center content-center">
         <input
             type="checkbox"
-            @checked($is_completed)
+            wire:model.live.debounce.500ms="isCompleted"
+            @checked($isCompleted)
             class="checkbox checkbox-primary ml-1"
         />
         <button class="delete-checklist-item-btn btn btn-error btn-xs btn-square btn-outline ml-1">
