01b15c2bf364227dfa904acb2605c81086d4b4ea
KonKeRoR <trgkien.vu@gmail.com>
2025-04-11 17:36:03 +0700

    feat: Add keyword binding and empty search method
    Added a `keyword` property with `wire:model` binding to capture input data. Introduced an empty `search` method as a placeholder for future logic. Updated the input field to trigger an alert on "Enter" key press.

--- resources/views/livewire/note/quick-search-form-livewire.blade.php
+++ resources/views/livewire/note/quick-search-form-livewire.blade.php
@@ -5,9 +5,16 @@ use Livewire\Volt\Component;
 new class extends Component {
     public bool $fullWidth;
 
+    public string $keyword = '';
+
     public function mount(bool $fullWidth = false): void
     {
         $this->fullWidth = $fullWidth;
+    }
+
+    public function search(): void
+    {
+
     }
 }; ?>
 
@@ -20,6 +27,6 @@ new class extends Component {
     'ml-2',
     'w-full' => $fullWidth,
 ])>
-    <input type="text" class="grow" placeholder="Search"/>
+    <input wire:model="keyword" @keyup.enter="alert('Submitted!')" type="text" class="grow" placeholder="Search"/>
     <x-ionicon-search class="h-4 w-4"/>
 </label>
