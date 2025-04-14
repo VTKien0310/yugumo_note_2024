6780be6e4ced6797cba3bc7ffc3a8b7eeefae4e0
KonKeRoR <trgkien.vu@gmail.com>
2025-04-14 17:02:27 +0700

    feat: Add "Frequently viewed" section to home note page
    Introduced a new section to display frequently viewed notes below the recently viewed notes. This enhances the user's ability to quickly access popular or commonly accessed notes. Minor layout adjustments were made to improve the overall design.

--- resources/views/pages/note/home-note-page.blade.php
+++ resources/views/pages/note/home-note-page.blade.php
@@ -6,11 +6,21 @@
 
     <x-layouts.authenticated-layout>
         <div class="flex flex-col justify-center items-center">
-            <div class="w-full">
+            <div class="w-full mt-5">
                 <h1 class="w-full text-start text-2xl pl-5">Recently viewed</h1>
                 <div class="w-full grid grid-cols-3 gap-2 mb-5 px-5">
                     @php /** @var NoteListDisplayDataValueObject $note */ @endphp
                     @foreach($recentlyViewedNotes as $note)
+                        <x-note.home-note-card :note="$note"/>
+                    @endforeach
+                </div>
+            </div>
+            <div class="divider px-5"></div>
+            <div class="w-full mt-5">
+                <h1 class="w-full text-start text-2xl pl-5">Frequently viewed</h1>
+                <div class="w-full grid grid-cols-3 gap-2 mb-5 px-5">
+                    @php /** @var NoteListDisplayDataValueObject $note */ @endphp
+                    @foreach($trendingNotes as $note)
                         <x-note.home-note-card :note="$note"/>
                     @endforeach
                 </div>
