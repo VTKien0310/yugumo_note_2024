2d610f76f112cce8acc4826bedc770d0c648b1b5
KonKeRoR <trgkien.vu@gmail.com>
2025-04-14 17:08:21 +0700

    feat: Adjust grid layout for responsive design on note pages.
    Updated the grid layout from fixed 3 columns to a responsive layout with 1 column on small screens, 3 columns on medium screens, and 6 columns on extra-large screens. This improves usability and appearance across different device sizes.

--- resources/views/pages/note/home-note-page.blade.php
+++ resources/views/pages/note/home-note-page.blade.php
@@ -8,7 +8,7 @@
         <div class="flex flex-col justify-center items-center">
             <div class="w-full mt-5">
                 <h1 class="w-full text-start text-2xl pl-5">Recently viewed</h1>
-                <div class="w-full grid grid-cols-3 gap-2 mb-5 px-5">
+                <div class="w-full grid grid-cols-1 md:grid-cols-3 xl:grid-cols-6 gap-2 mb-5 px-5">
                     @php /** @var NoteListDisplayDataValueObject $note */ @endphp
                     @foreach($recentlyViewedNotes as $note)
                         <x-note.home-note-card :note="$note"/>
@@ -18,7 +18,7 @@
             <div class="divider px-5"></div>
             <div class="w-full mt-5">
                 <h1 class="w-full text-start text-2xl pl-5">Frequently viewed</h1>
-                <div class="w-full grid grid-cols-3 gap-2 mb-5 px-5">
+                <div class="w-full grid grid-cols-1 md:grid-cols-3 xl:grid-cols-6 gap-2 mb-5 px-5">
                     @php /** @var NoteListDisplayDataValueObject $note */ @endphp
                     @foreach($trendingNotes as $note)
                         <x-note.home-note-card :note="$note"/>
