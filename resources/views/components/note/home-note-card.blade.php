b1d3dcb8f86442884a97989512e381b0d353a734
KonKeRoR <trgkien.vu@gmail.com>
2025-04-14 16:58:31 +0700

    feat: Add `home-note-card` component for displaying notes
    Created a reusable Blade component for rendering note cards to simplify the code and improve maintainability. Updated the `home-note-page` view to use this new component when displaying recently viewed notes.

---
+++ resources/views/components/note/home-note-card.blade.php
@@ -0,0 +1,21 @@
+@php
+    use App\Features\Note\ValueObjects\NoteListDisplayDataValueObject;
+@endphp
+
+@props(['note'])
+
+@php /** @var NoteListDisplayDataValueObject $note */ @endphp
+<div class="card bg-base-100 w-full h-full shadow-xl">
+    <div class="card-body">
+        <div class="badge badge-ghost">{{ $note->type }}</div>
+        <h2 class="card-title">{{ $note->shortTitle }}</h2>
+        <p>{{ $note->shortContent }}</p>
+        <div class="card-actions justify-end">
+            <a href="{{ route('notes.show', ['note' => $note->id]) }}">
+                <button class="btn btn-sm btn-square btn-primary">
+                    <x-ionicon-information class="h-4 w-4"/>
+                </button>
+            </a>
+        </div>
+    </div>
+</div>
--- resources/views/pages/note/home-note-page.blade.php
+++ resources/views/pages/note/home-note-page.blade.php
@@ -7,24 +7,11 @@
     <x-layouts.authenticated-layout>
         <div class="flex flex-col justify-center items-center">
             <div class="w-full">
-                <h1 class="w-full text-start text-2xl">Recently viewed</h1>
+                <h1 class="w-full text-start text-2xl pl-5">Recently viewed</h1>
                 <div class="w-full grid grid-cols-3 gap-2 mb-5 px-5">
                     @php /** @var NoteListDisplayDataValueObject $note */ @endphp
                     @foreach($recentlyViewedNotes as $note)
-                        <div class="card bg-base-100 w-full h-full shadow-xl">
-                            <div class="card-body">
-                                <div class="badge badge-ghost">{{ $note->type }}</div>
-                                <h2 class="card-title">{{ $note->shortTitle }}</h2>
-                                <p>{{ $note->shortContent }}</p>
-                                <div class="card-actions justify-end">
-                                    <a href="{{ route('notes.show', ['note' => $note->id]) }}">
-                                        <button class="btn btn-sm btn-square btn-primary">
-                                            <x-ionicon-information class="h-4 w-4"/>
-                                        </button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+                        <x-note.home-note-card :note="$note"/>
                     @endforeach
                 </div>
             </div>
