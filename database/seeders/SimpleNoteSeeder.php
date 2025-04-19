6cd9b700d65c1094ff80aec084c78bc3c924434b
VTKien0310 <trgkien.vu@gmail.com>
2025-04-19 22:14:59 +0700

    ref: Refactor SimpleNoteSeeder to use action classes.
    Updated the seeder to utilize CreateNewNoteWithDefaultContentAction and UpdateNoteAction for a cleaner and more modular implementation. This change improves maintainability and leverages established actions for note creation and updates.

--- database/seeders/SimpleNoteSeeder.php
+++ database/seeders/SimpleNoteSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Features\Note\Actions\CreateNewNoteWithDefaultContentAction;
+use App\Features\Note\Actions\UpdateNoteAction;
 use App\Features\Note\Models\Note;
 use App\Features\Note\Models\TextNoteContent;
 use App\Features\NoteType\Enums\NoteTypeEnum;
+use App\Features\NoteType\Models\NoteType;
 use App\Features\User\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -13,19 +16,31 @@ class SimpleNoteSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
+    public function run(
+        CreateNewNoteWithDefaultContentAction $createNewNoteWithDefaultContentAction,
+        UpdateNoteAction $updateNoteAction
+    ): void {
         $user = User::query()->where(User::EMAIL, 'trgkien.vu@gmail.com')->first();
 
-        collect(range(1, 200))->each(function () use ($user) {
-            $note = $user->notes()->create([
-                Note::TYPE_ID => NoteTypeEnum::SIMPLE->value,
-                Note::TITLE => fake()->sentence(),
-            ]);
+        collect(range(1, 200))->each(
+            function () use (
+                $createNewNoteWithDefaultContentAction,
+                $updateNoteAction,
+                $user
+            ): void {
+                $note = $createNewNoteWithDefaultContentAction->handle(
+                    user: $user,
+                    noteType: NoteType::query()->where(NoteType::ID, NoteTypeEnum::SIMPLE->value)->first()
+                );
 
-            $note->textContent()->create([
-                TextNoteContent::CONTENT => fake()->paragraph(),
-            ]);
-        });
+                $updateNoteAction->handle($note, [
+                    Note::TITLE => fake()->sentence(),
+                ]);
+
+                $note->textContent()->update([
+                    TextNoteContent::CONTENT => fake()->paragraph(),
+                ]);
+            }
+        );
     }
 }
