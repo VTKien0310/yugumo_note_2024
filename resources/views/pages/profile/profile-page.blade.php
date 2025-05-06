071c1d94cbdc0516e5f2448d6785c1c9354a6d84
KonKeRoR <trgkien.vu@gmail.com>
2025-05-06 13:57:08 +0700

    feat: Add padding to profile page container
    Added padding (`p-3`) to the main container of the profile page for better spacing and visual consistency. This ensures improved alignment and enhances the overall layout.

--- resources/views/pages/profile/profile-page.blade.php
+++ resources/views/pages/profile/profile-page.blade.php
@@ -3,7 +3,7 @@
 
     <x-layouts.authenticated-layout>
         <div class="w-full flex flex-row items-center justify-center">
-            <div class="w-full md:w-1/2 lg:w-1/4 xl:w-1/4 flex flex-col items-center justify-start">
+            <div class="w-full md:w-1/2 lg:w-1/4 xl:w-1/4 p-3 flex flex-col items-center justify-start">
                 <livewire:profile-form-livewire/>
                 <div class="divider"></div>
                 <x-form class="w-full" action="{{ route('auth.logout') }}" method="DELETE">
