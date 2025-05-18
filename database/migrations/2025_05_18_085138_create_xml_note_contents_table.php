6224cc1cfac02d446982731da05fce1f176b2aab
VTKien0310 <trgkien.vu@gmail.com>
2025-05-18 15:58:26 +0700

    feat: Add XML note content model and migration
    Introduced `XmlNoteContent` model and its corresponding migration to manage XML-based note content. Integrated relationships between `Note` and `XmlNoteContent`, along with search indexing capabilities for the new model.

---
+++ database/migrations/2025_05_18_085138_create_xml_note_contents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('xml_note_contents', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->foreignUlid('note_id')
+                ->constrained('notes')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->longText('content');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('xml_note_contents');
+    }
+};
