44f939f358cc685f2d7a84b6ff8b69fc9198167c
VTKien0310 <trgkien.vu@gmail.com>
2025-05-25 16:45:19 +0700

    feat: Add XML note editing functionality
    This commit introduces support for editing XML notes, including a dedicated Blade component and functionality for handling XML content. The Ace editor library has been added to the project for XML content editing, with its required dependencies integrated into the build system.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,10 @@
 import './bootstrap';
+import ace from 'ace-builds'
+import 'ace-builds/src-noconflict/mode-xml';
+import 'ace-builds/src-noconflict/theme-monokai';
 
 import.meta.glob([
     '../images/**',
 ]);
+
+window.ace = ace;
---
+++ resources/views/livewire/note/edit-xml-note-form-livewire.blade.php
@@ -0,0 +1,72 @@
+<?php
+
+use Livewire\Volt\Component;
+use App\Features\Note\Models\Note;
+use App\Features\Note\Actions\UpdateNoteAction;
+
+new class extends Component {
+    public Note $note;
+
+    public string $title;
+
+    public string $content;
+
+    public string $description;
+
+    public function mount(Note $note): void
+    {
+        $this->note = $note;
+        $this->title = $note->title;
+        $this->content = $note->xmlContent->content;
+        $this->description = $note->textContent->content;
+    }
+
+    public function updated(): void
+    {
+        app()->make(UpdateNoteAction::class)->handle($this->note, [
+            'title' => $this->title,
+            'text_content' => $this->description,
+            'xml_content' => $this->content,
+        ]);
+    }
+}; ?>
+
+<div class="w-3/4 xl:w-1/2">
+    <div class="w-full flex flex-col justify-start items-center">
+
+        <div class="w-full flex flex-col justify-start items-start mb-5">
+            <x-label for="title" class="font-bold text-xs mb-1"/>
+            <x-input wire:model.live.debounce.500ms="title" name="title" class="input input-bordered w-full"/>
+        </div>
+
+        <div class="w-full flex flex-col justify-start items-start mb-5">
+            <x-label for="description" class="font-bold text-xs mb-1"/>
+            <x-input
+                wire:model.live.debounce.500ms="description"
+                name="description"
+                class="input input-bordered w-full"
+            />
+        </div>
+
+        <div class="w-full flex flex-col justify-start items-start">
+            <x-label for="content" class="font-bold text-xs mb-1"/>
+            {{-- hidden input to store xml content from Ace editor --}}
+            <x-textarea
+                wire:model.live.debounce.500ms="content"
+                name="content"
+                id="content-input"
+                class="hidden"
+            />
+            {{-- load Ace editor using Alpine.js --}}
+            <div class="w-full" wire:ignore x-data x-init="
+                const editor = ace.edit($refs.e);
+                editor.setTheme('ace/theme/monokai');
+                editor.session.setMode('ace/mode/xml');
+                editor.setValue(@entangle('content').defer, -1);
+                editor.session.on('change', () => @this.set('content', ed.getValue()));
+            ">
+                <div x-ref="e" class="w-full" style="height: 400px; border:1px solid #ddd;"></div>
+            </div>
+        </div>
+    </div>
+</div>
